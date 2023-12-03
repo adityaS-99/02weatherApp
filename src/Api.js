d3621613cf75be5114cd8d0a27afad47aa2881ef
Aditya <00007adi@gmail.com>
2023-12-03 21:19:50 +0530

    Weather app

---
+++ src/Api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": 'e168cf6805msh7a1479a56d16ce5p166fabjsn9f85fc29e5a7',// enter your rapid api key here
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "2bd716c42e8cc96f47a3c2934f35f247"; // enter your key from openweather API
